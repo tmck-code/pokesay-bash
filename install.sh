7a4c05aac7a7500264cfa16d4049e9b845a394b2
Tom McKeesick <tmck01@gmail.com>
2019-12-17 00:57:27 +1100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Define install directories and names
+install_path="$HOME/.pokemonsay"
+bin_path="$HOME/bin"
+pokemonsay_bin="pokemonsay"
+
+# Make sure the directories exist
+mkdir -p $install_path/
+mkdir -p $install_path/cows/
+mkdir -p $bin_path/
+
+# Copy the cows and the main script to the install path.
+cp ./cows/*.cow $install_path/cows/
+cp ./pokemonsay.sh $install_path/
+
+# Create the pokemonsay script in the home bin directory.
+cat > $bin_path/$pokemonsay_bin <<- EOF
+	#!/bin/sh
+
+	# This script changes to the pokemonsay installation directory,
+	# runs the main script for running the pokemonsay, and changes
+	# back to the previous directory.
+
+	cd $install_path/
+	./pokemonsay.sh \$@
+	cd - >/dev/null
+EOF
+
+# Create uninstall script in the install directory
+cat > $install_path/uninstall.sh <<- EOF
+	#!/bin/sh
+
+	#
+	# This script uninstalls pokemonsay.
+	#
+
+	# Remove the install directory
+	rm -r "$install_path/"
+
+	# Remove the bin files
+	rm "$bin_path/$pokemonsay_bin"
+
+	# Say what's going on.
+	echo "'$install_path/' directory was removed."
+	echo "'$bin_path/$pokemonsay_bin' file was removed."
+EOF
+
+# Change permission of the generated scripts
+chmod +x "$bin_path/$pokemonsay_bin"
+chmod +x "$install_path/uninstall.sh"
+
+echo "The files were installed to '$install_path/'."
+echo "A '$pokemonsay_bin' script was created in '$bin_path/'."
+echo "A uninstall script was created in '$install_path/'."
+echo "It may be necessary to logout and login back again in order to have the '$pokemonsay_bin' available in your path."
---
+++ pokemonsay.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+usage() {
+  echo "
+  Description: Prints a Pokémon with a message.
+  Usage:
+	$(basename $0) [-p POKÉMON] [-f COWFILE] [-w COLUMN] [-lnNt] [-h] [MESSAGE]
+
+  Options:
+    -p, --pokemon POKÉMON_NAME
+      CHOOSE a Pokémon by name.
+    -f, --file COWFILE
+      CHOOSE a Pokémon cowfile.
+    -w, --word-wrap COLUMN
+      Specifies roughly where the message should be wrapped.
+    -n, --no-wrap
+      Do not wrap the message.
+    -l, --list
+      List all the Pokémon available.
+    -N, --no-name
+      Do not print the name of the Pokémon.
+    -t, --think
+      Make the Pokémon think the message instead of saying it.
+    -h, --help
+      Display this usage message.
+    message
+      The message for chosen Pokémon to say. If this is not provided the Pokémon will read from STDIN
+"
+  exit 0
+}
+
+list_pokemon() {
+	echo "Pokémon available in '$POKEMON/':"
+	ls -1 ./cows/ | sed 's/\.cow$//g'
+	exit 0
+}
+
+# Disable wrapping by default
+WORD_WRAP="-n"
+
+while getopts ":p:f:wnNlh" Option ; do
+	case $Option in
+		f ) COWFILE="$OPTARG" ;;
+		p ) I_CHOOSE="$OPTARG" ;;
+		w ) WORD_WRAP="-W $OPTARG" ;;
+		n ) DISABLE_WRAP=true   ;;
+		N ) DISABLE_NAME=true   ;;
+		l ) list_pokemon        ;;
+		h ) usage               ;;
+		* ) echo "Unimplemented option chosen." && usage ;;
+	esac
+done
+
+shift $(($OPTIND - 1))
+MESSAGE="${1}"
+
+# Define which pokemon should be displayed, then call cowsay or cowthink
+# TODO: restore cowthink after fine-tuning cowsay
+if [ -n "$I_CHOOSE" ]; then
+	cowsay -f "$PWD/cows/$I_CHOOSE.cow" $WORD_WRAP "$MESSAGE"
+elif [ -n "$COW_FILE" ]; then
+	cowsay -f "$COW_FILE" $WORD_WRAP "$MESSAGE"
+else
+	a=(cows/*)
+	I_CHOOSE=${a[$((RANDOM % ${#a[@]}))]}
+	I_CHOOSE=${I_CHOOSE#cows/}
+	I_CHOOSE=${I_CHOOSE%.cow}
+	cowsay -f $PWD/cows/$I_CHOOSE.cow $WORD_WRAP "$MESSAGE"
+fi
+
+# Write the pokemon name, unless requested otherwise.
+[ -n "$DISABLE_NAME" ] && echo $I_CHOOSE
+
