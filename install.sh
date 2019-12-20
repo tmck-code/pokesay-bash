c114e413866819c4201ecc34d85728322f7c2f87
Tom McKeesick <tmck01@gmail.com>
2019-12-20 22:18:11 +1100

    Rename pokemonsay executable without .sh extension

--- install.sh
+++ install.sh
@@ -13,28 +13,16 @@ mkdir -p $bin_path/
 # Copy the cows and the main script to the install path.
 tar xzf cows.tar.gz -C $install_path/
 N_POKEMON=$(find $install_path/ -type f -name *.cow | wc -l)
-echo "Copied $N_POKEMON Pokemon to install path '$install_path'"
-cp ./pokemonsay.sh $install_path/
-
-# Create the pokemonsay script in the home bin directory.
-cat > $bin_path/$pokemonsay_bin <<- EOF
-	#!/bin/sh
-
-	# This script changes to the pokemonsay installation directory,
-	# runs the main script for running the pokemonsay, and changes
-	# back to the previous directory.
-
-	cd $install_path/
-	./pokemonsay.sh \$@
-	cd - >/dev/null
-EOF
+echo -e "\nCopied $N_POKEMON Pokémon to install path '$install_path'"
+cp ./pokemonsay $bin_path/$pokemonsay_bin/
 
 # Change permission of the generated scripts
 chmod +x "$bin_path/$pokemonsay_bin"
 
 echo "\
+
 - The files were installed to '$install_path/'.
 - A '$pokemonsay_bin' script was created in '$bin_path/'.
-- A uninstall script was created in '$install_path/'.
+
 It may be necessary to logout and login back again in order to have the '$pokemonsay_bin' available in your path.
 "
