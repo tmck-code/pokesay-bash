e68c7671d530545be3c836ee1c491a6ff988e235
Tom McKeesick <tmck01@gmail.com>
2019-12-31 14:29:00 +1100

    Use -E flag for sed, and put all cowfiles in one flat dir for now

--- ops/build.sh
+++ ops/build.sh
@@ -7,7 +7,9 @@ odir="${2}"
 sprites="$(find ${idir} -type f -name *.png)"
 find ${idir} -type d | parallel -I {} mkdir -p "${odir}/{}"
 
+mkdir /tmp/cows
 for f in $(echo "${sprites}"); do
-  img2xterm -c "${f}" | sed '/^[[:space:]]*$/d' | sed 's/^    //g' > "${odir}/${f%.png}.cow"
+  img2xterm -c "${f}" | sed '/^[[:space:]]*$/d' | sed -E 's/^[[:space:]]{1-6}//g' > "${odir}/${f%.png}.cow"
+  mv "${odir}/${f%.png}.cow" /tmp/cows
 done
 
